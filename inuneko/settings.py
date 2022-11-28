baaca7a2c9875811c4a2c95e824bb0c7403ff4be
aohana-AO <kbc21a34@stu.kawahara.ac.jp>
2022-11-28 10:34:40 +0900

    templateも作成、一番基礎の状況まで。（accounts周りでインポートエラーのためurls.pyコメントアウト）

--- inuneko/settings.py
+++ inuneko/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
+    'accounts',
+    'allauth',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'inuneko.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'app/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
