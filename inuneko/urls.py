02351f8138ed2b20d95605078501f15752ff7db3
aohana-AO <kbc21a34@stu.kawahara.ac.jp>
2022-12-13 10:06:28 +0900

    login,logoutページの作成

--- inuneko/urls.py
+++ inuneko/urls.py
@@ -21,6 +21,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-    #path('accounts/', include('accounts.urls')),
-    #path('accounts/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
